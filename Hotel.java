72d03fcc29473302e9b6e7c51496239fdb14738c
TIFOURAmanel <maneltifoura@gmail.com>
2024-03-22 16:42:40 +0100

    manel's commit

--- Hotel.java
+++ Hotel.java
@@ -1,4 +1,32 @@
+import java.util.HashMap;
+
+
+
 public class Hotel {
+	
+	
+	
+	public static HashMap< String ,Client> clients = new HashMap<String, Client> ();
+	 public static HashMap<Integer, Chambre> chambres = new HashMap<Integer, Chambre> ();
+	public static HashMap<Integer , User> users = new HashMap<Integer, User> ();
+	
+	
+	public static HashMap<Integer, Chambre> getchambres() {
+		// TODO Auto-generated method stub
+		return chambres;
+	}
+
+
+	public static HashMap<Integer, User> getusers() {
+		// TODO Auto-generated method stub
+		return users;
+	}
+
+
+	public static HashMap<String, Client> getclients() {
+		// TODO Auto-generated method stub
+		return clients;
+	}
     public static void main(String[] args) {
     }
 }
